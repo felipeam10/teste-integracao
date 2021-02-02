9401410ca3deae2bdc6d687a01364bd3f41b1979
felipe.araujo <felipe.araujo@sankhya.com.br>
2021-02-01 22:17:32 -0300

    Criando e organizando os testes

--- src/test/java/br/com/felipe/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/felipe/leilao/dao/UsuarioDaoTest.java
@@ -3,8 +3,11 @@ package br.com.felipe.leilao.dao;
 import static org.junit.jupiter.api.Assertions.*;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.felipe.leilao.model.Usuario;
@@ -13,19 +16,40 @@ import br.com.felipe.leilao.util.JPAUtil;
 class UsuarioDaoTest {
 
 	private UsuarioDao dao;
+	private EntityManager em;
+	
+	@BeforeEach
+	public void beforeEach() {
+		this.em = JPAUtil.getEntityManager();
+		this.dao = new UsuarioDao(em); //para nao dar NPE temos que instanciar a classe DAO no teste. temos que passar um em como parametro para simular a injecao de dependencias
+		em.getTransaction().begin();
+	
+	}
+	
+	@AfterEach
+	public void afterEach() {
+		em.getTransaction().rollback();
+	}
 	
 	@Test
-	void testeBuscaPeloUsuarioDeUsername() {
-		EntityManager em = JPAUtil.getEntityManager();
-		this.dao = new UsuarioDao(em); //para nao dar NPE temos que instanciar a classe DAO no teste. temos que passar um em como parametro para simular a injecao de dependencias
-		
-		Usuario usuario = new Usuario("fulano", "fulano@gmail.com", "12345678");
-		em.getTransaction().begin();
-		em.persist(usuario);
-		em.getTransaction().commit();
+	void deveriaEncontrarUsuarioCadastrado() {
+		Usuario usuario = criarUsuario();
 		
 		Usuario usuarioEncontrado = this.dao.buscarPorUsername(usuario.getNome());
 		Assert.assertNotNull(usuarioEncontrado);
+	}
+	
+	
+	@Test
+	void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+		criarUsuario();
+		Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("beltrano"));
+	}
+	
+	private Usuario criarUsuario() {
+		Usuario usuario = new Usuario("fulano", "fulano@gmail.com", "12345678");
+		em.persist(usuario);
+		return usuario;
 	}
 
 }
